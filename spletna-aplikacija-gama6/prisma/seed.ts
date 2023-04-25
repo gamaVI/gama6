f69168b6649f6cdb197f62349704ffa16a30a60c
galjeza <gal.jeza@protonmail.com>
2023-04-25 20:08:13 +0200

    GAM-43 Json file za testne podatke

--- spletna-aplikacija-gama6/prisma/seed.ts
+++ spletna-aplikacija-gama6/prisma/seed.ts
@@ -1,33 +1,22 @@
 import { prisma } from "../src/server/db";
+import seedData from "./seeddata.json";
+
+
 
 async function main() {
-  const posel = await prisma.posel.create({
-    data: {
-      skupnaPovrsina: 1456.67,
-      cena: 123456,
-      cenaNaM2: 13.5,
-      lokacija: "Ljubljana",
-      datumPosla: new Date(),
-      letoGradnje: 1999,
-      tipPosla: "Prodaja",
-      tipNepremicnine: "Stanovanje",
-      koordinataX: 46.056946,
-      koordinataY: 14.505751,
-    },
-  });
-  const oglas = await prisma.oglas.create({
-    data: {
-      url: "https://www.nepremicnine.net/oglasi-prodaja/ljubljana-mesto-stanovanje_6117937/",
-      naslov: "Ljubljana, Center, 2-sobno stanovanje, 50 m2",
-      tip: "Stanovanje",
-      opis: "Prodamo 2-sobno stanovanje v centru Ljubljane, na odlični lokaciji, v bližini vse infrastrukture. Stanovanje se nahaja v 1. nadstropju urejene večstanovanjske stavbe zgrajene leta 1930. Stanovanje je bilo v celoti obnovljeno leta 2018. Stanovanje obsega: hodnik, kopalnico, spalnico, kuhinjo z jedilnico in dnevno sobo. Stanovanje je svetlo in funkcionalno razporejeno. Stanovanje je opremljeno in vseljivo po dogovoru. Vseljivo takoj. Vabljeni na ogled!",
-      velikost: 50,
-      cena: 123456,
-      agencija: "ABC nepremičnine",
-      lokacija: "Ljubljana",
-    },
-  });
-  console.log({ posel, oglas });
+    // Create posel data
+    for (const poselData of seedData.posel) {
+      await prisma.posel.create({
+        data: poselData
+      })
+    }
+  
+    // Create oglas data
+    for (const oglasData of seedData.oglas) {
+      await prisma.oglas.create({
+        data: oglasData
+      })
+    }
 }
 
 main()
