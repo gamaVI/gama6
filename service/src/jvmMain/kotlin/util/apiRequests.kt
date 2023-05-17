34a55bdd29b374ec6de05e399fb817e8c55ac332
Matic Lukežič <31586745+mlukee@users.noreply.github.com>
2023-05-17 15:19:44 +0200

    GAM-80 Povezava z scraperjem preko API

---
+++ service/src/jvmMain/kotlin/util/apiRequests.kt
@@ -0,0 +1,57 @@
+package util
+
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import util.objects.Listing
+
+data class ListingsResponse(val listings: List<Listing>, val pageCount: Int)
+
+object ApiRequests {
+    private var client = OkHttpClient()
+    private var BASE_URL = "http://localhost:3000/api/scraping/nepremicnine"
+
+    @JvmStatic
+    fun getNepremicnine(pageNumber: Int): ListingsResponse {
+        try {
+            val formBody = FormBody.Builder()
+                .add("pageNumber", pageNumber.toString())
+                .add("minCost", "1")
+                .add("maxCost", "99999999")
+                .add("typeOfBuilding", "stanovanje")
+                .add("location", "ljubljana-mesto")
+                .build()
+
+            val request = Request.Builder()
+                .url(BASE_URL)
+                .post(formBody)
+                .build()
+
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    throw IOException("Unexpected code $response")
+                }
+
+                val responseData = response.body?.string()
+                responseData?.let {
+                    val jsonObject = JsonParser.parseString(it).asJsonObject
+                    val listingsJsonArray = jsonObject.getAsJsonArray("listings")
+                    val pageCount = jsonObject.getAsJsonPrimitive("pageCount").asInt
+
+                    val gson = Gson()
+                    val listings = gson.fromJson(listingsJsonArray, Array<Listing>::class.java)
+
+                    return ListingsResponse(listings.toList(), pageCount)
+                }
+            }
+        } catch (e: Exception) {
+            println(e)
+        }
+        return ListingsResponse(emptyList(), 1)
+    }
+
+}
+
