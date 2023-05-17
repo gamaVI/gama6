a9fbfe07743ef3c58d96c9c5b826350873207e1d
Matic Lukežič <31586745+mlukee@users.noreply.github.com>
2023-05-17 15:22:12 +0200

    GAM-80 UI za izpis nepremicnin

--- service/src/jvmMain/kotlin/util/screens/Nepremicnine.kt
+++ service/src/jvmMain/kotlin/util/screens/Nepremicnine.kt
@@ -1,17 +1,141 @@
 package util.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExtendedFloatingActionButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import util.ApiRequests
+import util.ListingsResponse
+import util.objects.Listing
+import util.ui.NepremicninaItem
+
+fun handleGetNepremicnine(apiResponse: MutableState<ListingsResponse>, pageNumber: Int = 1) {
+    try {
+        apiResponse.value = ApiRequests.getNepremicnine(pageNumber)
+    } catch (e: Exception) {
+        println(e)
+    }
+    println("Test connection")
+}
+
 
 @Composable
-fun NepremicnineScreen(){
-    Column (modifier = Modifier.fillMaxSize()) {
-        Box {
-            Text("Nepremicnine Screen")
+fun NepremicnineScreen() {
+    var loading by remember { mutableStateOf(false) }
+    val apiResponse = remember { mutableStateOf(ListingsResponse(emptyList(), 1)) }
+    val pageNumber = remember { mutableStateOf(1) }
+    val state = rememberLazyListState()
+    val listings = remember { mutableStateOf(mutableListOf<Listing>()) }
+
+    //Handle async requests
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            "Nepremičnine",
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.h2
+        )
+        if (loading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
         }
+        if (!loading && listings.value.isNotEmpty()) {
+            Box(modifier = Modifier.weight(1f)) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    state = state
+                ) {
+                    item {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            listings.value.forEachIndexed { index, nepremicnina ->
+                                NepremicninaItem(
+                                    nepremicnina = nepremicnina,
+                                    onDelete = {
+                                        listings.value = listings.value.toMutableList().apply {
+                                            removeAt(index)
+                                        }
+                                    },
+                                    onEdit = { editedNepremicnina: Listing ->
+                                        listings.value = listings.value.toMutableList().apply {
+                                            this[index] = editedNepremicnina
+                                        }
+                                    }
+                                )
+                                Spacer(modifier = Modifier.height(8.dp))
+                            }
+                        }
+                    }
+                }
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight().padding(start = 5.dp),
+                    adapter = rememberScrollbarAdapter(scrollState = state)
+                )
+                if (loading) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                } else {
+                    ExtendedFloatingActionButton(
+                        text = { Text("Prikaži več") },
+                        onClick = {
+                            loading = true
+                            coroutineScope.launch {
+                                try {
+                                    withContext(Dispatchers.IO) {
+                                        handleGetNepremicnine(apiResponse, pageNumber.value)
+                                        listings.value.addAll(apiResponse.value.listings)
+                                        pageNumber.value += 1
+                                    }
+                                } catch (e: Exception) {
+                                    println(e)
+                                } finally {
+                                    loading = false
+                                }
+                            }
+                        },
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+
+        }
+        if (!loading && listings.value.isEmpty()) {
+            ExtendedFloatingActionButton(
+                text = { Text("Pridobi nepremičnine") },
+                onClick = {
+                    loading = true
+                    coroutineScope.launch {
+                        try {
+                            withContext(Dispatchers.IO) {
+                                handleGetNepremicnine(apiResponse, pageNumber.value)
+                                listings.value = apiResponse.value.listings.toMutableList()
+                                pageNumber.value += 1
+                            }
+                        } catch (e: Exception) {
+                            println(e)
+                        } finally {
+                            loading = false
+                        }
+                    }
+                },
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
     }
 }
