365932f6520f77b3b07f8c27d77028bc0b3efb7a
Matic Lukežič <31586745+mlukee@users.noreply.github.com>
2023-05-22 20:30:10 +0200

    API endpoint za Sparkasse posle

--- service/src/jvmMain/kotlin/util/apiRequests.kt
+++ service/src/jvmMain/kotlin/util/apiRequests.kt
@@ -1,22 +1,27 @@
 package util
 
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.io.IOException
+import Sparkasse
 import com.google.gson.Gson
 import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 import util.objects.Listing
+import util.objects.ListingsResponse
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-data class ListingsResponse(val listings: List<Listing>, val pageCount: Int)
 
 object ApiRequests {
+
     private val client = OkHttpClient.Builder()
         .callTimeout(25, TimeUnit.SECONDS) //Timeout after 25 seconds
         .readTimeout(25, TimeUnit.SECONDS)
         .build()
-    private var BASE_URL = "http://localhost:3000/api/scraping/nepremicnine"
+
+    private var NEPREMICNINE_API_ENDPOINT = "http://51.136.39.46:3000/api/scraping/nepremicnine"
+    private var SPARKASSE_API_ENDPOINT = "http://51.136.39.46:3000/api/scraping/posli"
 
     @JvmStatic
     fun getNepremicnine(pageNumber: Int): ListingsResponse {
@@ -30,7 +35,7 @@ object ApiRequests {
                 .build()
 
             val request = Request.Builder()
-                .url(BASE_URL)
+                .url(NEPREMICNINE_API_ENDPOINT)
                 .post(formBody)
                 .build()
 
@@ -55,6 +60,42 @@ object ApiRequests {
             println(e)
         }
         return ListingsResponse(emptyList(), 1)
+    }
+
+    @JvmStatic
+    fun getPosli(dateInterval: Int, selectedUnitTypeIndex: Int, selectedSubTypeIndex: Int): List<Sparkasse> {
+        try {
+            val json = """
+            {
+                "unit_type":  ${selectedUnitTypeIndex},
+                "unit_subtype": ${selectedSubTypeIndex},
+                "date_interval_months": $dateInterval
+            }
+            """.trimIndent()
+
+            val body = json.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+
+            val request = Request.Builder()
+                .url(SPARKASSE_API_ENDPOINT)
+                .post(body)
+                .build()
+
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    throw IOException("Unexpected code $response")
+                }
+
+                val responseData = response.body?.string()
+                responseData?.let {
+                    val gson = Gson()
+                    val type = object : TypeToken<List<Sparkasse>>() {}.type
+                    return gson.fromJson(it, type)
+                }
+            }
+        } catch (e: Exception) {
+            println(e)
+        }
+        return emptyList()
     }
 
 }
--- service/src/jvmMain/kotlin/util/ui/SparkasseItem.kt
+++ service/src/jvmMain/kotlin/util/ui/SparkasseItem.kt
@@ -1,2 +1,92 @@
 package util.ui
 
+import Sparkasse
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SparkasseItem(sparkasse: Sparkasse){
+    var isEditing by remember { mutableStateOf(false) }
+    val editedPosel = remember { mutableStateOf(sparkasse.copy()) }
+    Card(
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        elevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Tip: ${sparkasse.componentTypeDisplay}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Naslov: ${sparkasse.address}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Cena na m2: ${sparkasse.transactionAmountM2?.toString() ?: "N/A"}" ,
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Ocenjena cena na m2: ${sparkasse.estimatedAmountM2.toString()}",
+                )
+                Text(
+                    text = "X koordinata: ${sparkasse.gps.lng}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Y koordinata: ${sparkasse.gps.lat}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = sparkasse.transactionItemsList.toString(),
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text= "Item lists: ${sparkasse.transactionSumParcelSizes}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Datum transakcije: ${sparkasse.transactionDate}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Cena transakcije: ${sparkasse.transactionAmountGross}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Davek: ${sparkasse.transactionTax?.toString() ?: "N/A"} " ,
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Leto gradnje: ${sparkasse.buildingYearBuilt}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "St. sob: ${sparkasse.unitRoomCount?.toString() ?: "N/A"} " ,
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Stevilo kvadratov: ${sparkasse.unitRoomsSumSize}",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "Sobe: ${sparkasse.unitRooms}",
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
+    }
+
+}
