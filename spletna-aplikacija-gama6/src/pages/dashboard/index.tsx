763ef6f43ab9e7e775752c79f9938ed2a97ef040
Matic Lukežič <maticlukezic007@gmail.com>
2023-05-22 09:11:39 +0200

    Test secret webhook

--- spletna-aplikacija-gama6/src/pages/dashboard/index.tsx
+++ spletna-aplikacija-gama6/src/pages/dashboard/index.tsx
@@ -51,7 +51,7 @@ const DashboardPage: NextPage = () => {
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">
-              Nadzorna plošča test30
+              Nadzorna plošča
             </h2>
             <div className="flex items-center space-x-2"></div>
           </div>
