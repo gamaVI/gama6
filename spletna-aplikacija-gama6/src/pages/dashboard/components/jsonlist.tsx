e03582970eef8d30b47813d9b0e4d491afacb6a1
galjeza <gal.jeza@protonmail.com>
2023-05-24 18:39:42 +0200

    Auto stash before checking out "origin/FEATURE-desktopapp"

--- spletna-aplikacija-gama6/src/pages/dashboard/components/jsonlist.tsx
+++ spletna-aplikacija-gama6/src/pages/dashboard/components/jsonlist.tsx
@@ -17,7 +17,37 @@ const JsonList:React.FC<JsonListProps>  =({list}) => {
     <div>
       <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {list?.map((listItem) => (
-          <Card key={listItem?.id}>
+         <Card key={listItem.id}>
+         <CardHeader>
+           <CardTitle>{"Stanovanjska hiša"}</CardTitle>
+           <CardDescription>{listItem.address}</CardDescription>
+         </CardHeader>
+         <CardContent>
+           <Table>
+             <TableBody>
+               <TableRow>
+                 <TableCell>Transaction Amount M2</TableCell>
+                 <TableCell>{"90" }</TableCell>
+               </TableRow>
+               <TableRow>
+                 <TableCell>Estimated Amount M2</TableCell>
+                 <TableCell>{"100"}</TableCell>
+               </TableRow>
+                <TableRow>
+                  <TableCell>Leto gradnje</TableCell>
+                  <TableCell>{"1991"}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Leto gradnje</TableCell>
+                  <TableCell>{"1991"}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>{"Vrednost transakcije (bruto)"}</TableCell>
+                  <TableCell>{"123,123€"}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TransactionMarkerMap />
+                </TableRow>
 
             <CardContent className="overflow-scroll">
             <pre>
