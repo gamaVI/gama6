8755d63d15d36ddf5350cd124a14201e25711cc2
Matic Lukežič <maticlukezic007@gmail.com>
2023-12-07 17:07:06 +0100

    Create project

---
+++ rri-map/desktop/src/com/gama6/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.gama6.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.gama6.game.RRImap;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("RRI-map");
+		new Lwjgl3Application(new RRImap(), config);
+	}
+}
