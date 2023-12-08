e0ba427821e835eedb419f488f92fd922bb29d1e
galjeza <gal.jeza@protonmail.com>
2023-12-08 19:56:40 +0100

    remove tests

--- blockchain/chain-util.js
+++ blockchain/chain-util.js
@@ -1,34 +1,31 @@
-const EC = require('elliptic').ec;
-const ec = new EC('secp256k1');
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
 
-const uuidV1 = require('uuid/v1');
+const uuidV1 = require("uuid/v1");
 
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require("crypto-js/sha256");
 
-class ChainUtil{
+class ChainUtil {
+  static genKeyPair() {
+    return ec.genKeyPair();
+  }
 
-    static genKeyPair(){
-        return ec.genKeyPair();
-    }
+  static id() {
+    return uuidV1();
+  }
 
-    static id(){
-        return uuidV1();
-    }
+  static hash(data) {
+    return SHA256(JSON.stringify(data)).toString();
+  }
+  /**
+   * verify the transaction signature to
+   * check its validity using the method provided
+   * in EC module
+   */
 
-    static hash(data){
-        return SHA256(JSON.stringify(data)).toString();
-    }
-    /**
-     * verify the transaction signature to 
-     * check its validity using the method provided
-     * in EC module
-     */
-
-    static verifySignature(publicKey,signature,dataHash){
-        return ec.keyFromPublic(publicKey,'hex').verify(dataHash,signature);
-    }
-
-
+  static verifySignature(publicKey, signature, dataHash) {
+    return ec.keyFromPublic(publicKey, "hex").verify(dataHash, signature);
+  }
 }
 
 module.exports = ChainUtil;
