d0374bed688217e4c6c3ca4b0a3179505830d894
Matic Lukežič <maticlukezic007@gmail.com>
2024-01-12 18:55:55 +0100

    Fix some issues in Post Notification fragment when changing modes

--- mobile-application/app/src/main/java/com/example/gama6mobileapp/ui/post_a_message/PostAMessageFragment.kt
+++ mobile-application/app/src/main/java/com/example/gama6mobileapp/ui/post_a_message/PostAMessageFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.gama6mobileapp.databinding.FragmentPostAMessageBinding
+
 
 class PostAMessageFragment : Fragment() {
 
@@ -28,10 +31,41 @@ class PostAMessageFragment : Fragment() {
         _binding = FragmentPostAMessageBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        var messageType = ""
+
+        val spinner: Spinner = binding.postAMessageSpinnerMessageType
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (view != null) {
+                    messageType = parent.getItemAtPosition(position).toString()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                messageType = "Information"
+            }
         }
+
+        val sendButton = binding.postAMessageButtonPostMessage
+
+        sendButton.setOnClickListener {
+            val message = binding.postAMessageTextInputEditTextMessage.text.toString()
+            //create toast which will display the message type and message
+            binding.postAMessageTextInputEditTextMessage.text?.clear()
+            Toast.makeText(
+                context,
+                "Message type: $messageType\nMessage: $message",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        //TODO: Send message to server
+
         return root
     }
 
