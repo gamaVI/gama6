8cece4c4ec273adbc6a1a631baa0fad38ea7473b
Matic Lukežič <maticlukezic007@gmail.com>
2024-01-14 01:01:36 +0100

    Create new CameraFragment for taking images

---
+++ mobile-application/app/src/main/java/com/example/gama6mobileapp/ui/camera/CameraFragment.kt
@@ -0,0 +1,238 @@
+package com.example.gama6mobileapp.ui.camera
+
+import android.content.ContentValues
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.gama6mobileapp.databinding.FragmentCameraBinding
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.example.gama6mobileapp.R
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import kotlin.math.abs
+
+
+class CameraFragment : Fragment() {
+
+    private val args: CameraFragmentArgs by navArgs()
+    private var locationName: String? = null
+
+    private var _binding: FragmentCameraBinding? = null
+    private val binding get() = _binding!!
+
+    private val multiplePermissionId = 14
+    private val multiplePermissionNameList = arrayOf(
+        android.Manifest.permission.CAMERA,
+    )
+
+
+    private lateinit var imageCapture: ImageCapture
+    private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var camera: Camera
+    private lateinit var cameraSelector: CameraSelector
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        locationName = args.locationText
+
+        if (checkMultiplePermission()) {
+            startCamera()
+        }
+
+        binding.cameraCaptureButton.setOnClickListener {
+            takePhoto()
+        }
+
+        return root
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val name = SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS", Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Gama6")
+            }
+        }
+
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                requireContext().contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e("Gama6MobileAPP", "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = "Photo capture succeeded!"
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                    Log.d("Gama6MobileAPP", msg)
+                    findNavController().navigate(R.id.fromCameraToMyLocations)
+                }
+            }
+        )
+    }
+
+    private fun checkMultiplePermission(): Boolean {
+        val listPermissionNeeded = arrayListOf<String>()
+        for (permission in multiplePermissionNameList) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                listPermissionNeeded.add(permission)
+            }
+        }
+        if (listPermissionNeeded.isNotEmpty()) {
+            requestPermissions(
+                listPermissionNeeded.toTypedArray(),
+                multiplePermissionId
+            )
+            return false
+        }
+        return true
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == multiplePermissionId) {
+            if (grantResults.isNotEmpty()) {
+                var isGrant = true
+                for (element in grantResults) {
+                    if (element == PackageManager.PERMISSION_DENIED) {
+                        isGrant = false
+                        break
+                    }
+                }
+                if (isGrant) {
+                    // All permissions are granted
+                    startCamera()
+                } else {
+                    var someDenied = false
+                    for (permission in permissions) {
+                        if (!shouldShowRequestPermissionRationale(permission)) {
+                            someDenied = true
+                            break
+                        }
+                    }
+                    if (someDenied) {
+                        // Open app settings because some permissions are permanently denied
+                        appSettingOpen(requireContext())
+                    } else {
+                        // Show warning dialog for permission
+                        warningPermissionDialog(requireContext()) { dialog, which ->
+                            when (which) {
+                                DialogInterface.BUTTON_POSITIVE -> checkMultiplePermission()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            cameraProvider = cameraProviderFuture.get()
+            bindCameraUseCases()
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = maxOf(width, height).toDouble() / minOf(width, height)
+        return if (abs(previewRatio - 4.0 / 3.0) <= abs(previewRatio - 16.0 / 9.0)) {
+            AspectRatio.RATIO_4_3
+        } else AspectRatio.RATIO_16_9
+    }
+
+    private fun bindCameraUseCases() {
+        val screenAspectRatio =
+            aspectRatio(binding.cameraPreviewView.width, binding.cameraPreviewView.height)
+        val rotation = binding.cameraPreviewView.display.rotation
+
+        val resolutionSelector = ResolutionSelector.Builder().setAspectRatioStrategy(
+            AspectRatioStrategy(
+                screenAspectRatio,
+                AspectRatioStrategy.FALLBACK_RULE_AUTO
+            )
+        ).build()
+
+        val preview = Preview.Builder()
+            .setTargetRotation(rotation)
+            .setResolutionSelector(resolutionSelector)
+            .build()
+            .also {
+                it.setSurfaceProvider(binding.cameraPreviewView.surfaceProvider)
+            }
+
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+            .setResolutionSelector(resolutionSelector)
+            .setTargetRotation(rotation)
+            .build()
+
+        cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(lensFacing)
+            .build()
+
+        try {
+            cameraProvider.unbindAll()
+            camera = cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                preview,
+                imageCapture
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
