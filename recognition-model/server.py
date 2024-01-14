cb3921cc3dd16685e003f7bfd3718b018371d16c
galjeza <gal.jeza@protonmail.com>
2024-01-14 19:48:22 +0100

    api for ml model

---
+++ recognition-model/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import uvicorn
+from vehicle_count import *
+
+app = FastAPI()
+
+
+@app.post("/process-image/")
+async def process_image(file: UploadFile = File(...)):
+    with open("temp_image.jpg", "wb") as buffer:
+        buffer.write(file.file.read())
+
+    result = from_static_image("temp_image.jpg")
+    
+    return JSONResponse(content={"count": result})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=7000)
--- recognition-model/vehicle_count.py
+++ recognition-model/vehicle_count.py
@@ -8,14 +8,11 @@ import collections
 import numpy as np
 from tracker import *
 
-# Initialize Tracker
 tracker = EuclideanDistTracker()
 
-# Initialize the videocapture object
 cap = cv2.VideoCapture('video.mp4')
 input_size = 320
 
-# Detection confidence threshold
 confThreshold =0.2
 nmsThreshold= 0.2
 
@@ -23,17 +20,13 @@ font_color = (0, 0, 255)
 font_size = 0.5
 font_thickness = 2
 
-# Middle cross line position
 middle_line_position = 225   
 up_line_position = middle_line_position - 15
 down_line_position = middle_line_position + 15
 
 
-# Store Coco Names in a list
 classesFile = "coco.names"
 classNames = open(classesFile).read().strip().split('\n')
-print(classNames)
-print(len(classNames))
 
 # class index for our required detection classes
 required_class_index = [2, 3, 5, 7]
@@ -150,58 +143,6 @@ def postProcess(outputs,img):
         count_vehicle(box_id, img)
 
 
-def realTime():
-    while True:
-        success, img = cap.read()
-        img = cv2.resize(img,(0,0),None,0.5,0.5)
-        ih, iw, channels = img.shape
-        blob = cv2.dnn.blobFromImage(img, 1 / 255, (input_size, input_size), [0, 0, 0], 1, crop=False)
-
-        # Set the input of the network
-        net.setInput(blob)
-        layersNames = net.getLayerNames()
-        outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
-        # Feed data to the network
-        outputs = net.forward(outputNames)
-    
-        # Find the objects from the network output
-        postProcess(outputs,img)
-
-        # Draw the crossing lines
-
-        cv2.line(img, (0, middle_line_position), (iw, middle_line_position), (255, 0, 255), 2)
-        cv2.line(img, (0, up_line_position), (iw, up_line_position), (0, 0, 255), 2)
-        cv2.line(img, (0, down_line_position), (iw, down_line_position), (0, 0, 255), 2)
-
-        # Draw counting texts in the frame
-        cv2.putText(img, "Up", (110, 20), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Down", (160, 20), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Car:        "+str(up_list[0])+"     "+ str(down_list[0]), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Motorbike:  "+str(up_list[1])+"     "+ str(down_list[1]), (20, 60), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Bus:        "+str(up_list[2])+"     "+ str(down_list[2]), (20, 80), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Truck:      "+str(up_list[3])+"     "+ str(down_list[3]), (20, 100), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-
-        # Show the frames
-        cv2.imshow('Output', img)
-
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    # Write the vehicle counting information in a file and save it
-
-    with open("data.csv", 'w') as f1:
-        cwriter = csv.writer(f1)
-        cwriter.writerow(['Direction', 'car', 'motorbike', 'bus', 'truck'])
-        up_list.insert(0, "Up")
-        down_list.insert(0, "Down")
-        cwriter.writerow(up_list)
-        cwriter.writerow(down_list)
-    f1.close()
-    # print("Data saved at 'data.csv'")
-    # Finally realese the capture object and destroy all active windows
-    cap.release()
-    cv2.destroyAllWindows()
-
 
 image_file = 'test13.jpg'
 def from_static_image(image):
@@ -209,39 +150,22 @@ def from_static_image(image):
 
     blob = cv2.dnn.blobFromImage(img, 1 / 255, (input_size, input_size), [0, 0, 0], 1, crop=False)
 
-    # Set the input of the network
     net.setInput(blob)
     layersNames = net.getLayerNames()
     
     outputNames = [layersNames[i - 1] for i in net.getUnconnectedOutLayers()]
-    #outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
-    # Feed data to the network
     outputs = net.forward(outputNames)
 
-    # Find the objects from the network output
     postProcess(outputs,img)
 
-    # count the frequency of detected classes
     frequency = collections.Counter(detected_classNames)
-    print(frequency)
-    # Draw counting texts in the frame
-    cv2.putText(img, "Car:        "+str(frequency['car']), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-    cv2.putText(img, "Motorbike:  "+str(frequency['motorbike']), (20, 60), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-    cv2.putText(img, "Bus:        "+str(frequency['bus']), (20, 80), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-    cv2.putText(img, "Truck:      "+str(frequency['truck']), (20, 100), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
 
-
-    cv2.imshow("image", img)
-
-    cv2.waitKey(0)
-
-    # save the data to a csv file
-    with open("static-data.csv", 'a') as f1:
-        cwriter = csv.writer(f1)
-        cwriter.writerow([image, frequency['car'], frequency['motorbike'], frequency['bus'], frequency['truck']])
-    f1.close()
+    sum = frequency['car'] + frequency['motorbike'] + frequency['bus'] + frequency['truck']
+    return sum
 
 
 if __name__ == '__main__':
-    # realTime()
-    from_static_image(image_file)
+
+
+    result = from_static_image(image_file)
+    print(result)
